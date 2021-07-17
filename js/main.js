528c3520999010390ea2997315ec276b5c342d07
back-marker <61947228+back-marker@users.noreply.github.com>
2021-07-17 11:35:41 +0530

    Update version to v0.11.1

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function setRemainingTimeTimer(start_time, duration_min) {
 
 class Page {
   static SESSION_TYPE = { PRACTICE: "Practice", QUALIFYING: "Qualifying", RACE: "Race" }
-  static VERSION = "v0.11";
+  static VERSION = "v0.11.1";
 
   static cb_updateTeamsName(data) {
     if (data["status"] === "success") {
